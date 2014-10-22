a41b7313f8861ceea7acfc3777f28b4484a5aae2
Nischal HP <nischalhp@Nischals-MacBook-Air.local>
2014-10-22 15:40:54 +0530

    Similarity between product name
    * Trying to find the similarity between products

---
+++ cosine_distance.py
@@ -0,0 +1,85 @@
+import re, math
+from collections import Counter
+import csv
+import cosine_distance_objects
+
+WORD = re.compile(r'\w+')
+
+def get_cosine(vec1, vec2):
+     intersection = set(vec1.keys()) & set(vec2.keys())
+     numerator = sum([vec1[x] * vec2[x] for x in intersection])
+     sum1 = sum([vec1[x]**2 for x in vec1.keys()])
+     sum2 = sum([vec2[x]**2 for x in vec2.keys()])
+     denominator = math.sqrt(sum1) * math.sqrt(sum2)
+
+     if not denominator:
+        return 0.0
+     else:
+        return float(numerator) / denominator
+
+def text_to_vector(text):
+     words = WORD.findall(text)
+     return Counter(words)
+
+
+def read_text():
+
+    with open('/Users/nischalhp/Downloads/softwares/datasets/products_comparison/Redmart.csv','rU') as file_redmart:
+        redmart_csv = csv.reader(file_redmart,delimiter=',')
+        redmart_products = []
+        next(redmart_csv,None)
+        for row in redmart_csv:
+            cosine_obj = cosine_distance_objects()
+            cosine_obj.set_product_details(row[1].lower().strip(),row[2])
+            redmart_products.append(cosine_obj)
+
+    with open('/Users/nischalhp/Downloads/softwares/datasets/products_comparison/FairPrice.csv','rU') as file_fairprice:
+        fairprice_csv = csv.reader(file_fairprice,delimiter=',')
+        fairprice_products = []
+        next(fairprice_csv,None)
+        for row in fairprice_csv:
+            cosine_obj = cosine_distance_objects()
+            cosine_obj.set_product_details(row[1].lower().strip(),row[2])
+            fairprice_products.append(cosine_obj)
+
+
+    with open('/Users/nischalhp/Downloads/softwares/datasets/products_comparison/ColdStorage.csv','rU') as file_coldstorage:
+        coldstorage_csv = csv.reader(file_coldstorage,delimiter=',')
+        coldstorage_products = []
+        next(coldstorage_csv,None)
+        for row in coldstorage_csv:
+            cosine_obj = cosine_distance_objects()
+            cosine_obj.set_product_details(row[1].lower().strip(),row[2])
+            coldstorage_products.append(cosine_obj)
+
+
+	with open('/Users/nischalhp/Downloads/softwares/datasets/products_comparison/rm_fp.csv','w') as rm_fp_file:
+        for product in redmart_products:
+            for product_fp in fairprice_products:_
+                product_vector = text_to_vector(product)
+                product_fp_vector = text_to_vector(product_fp)
+                cosine_similarity = get_cosine(product_vector,product_fp_vector)
+                # just to get the ones with maximum similarity
+                if cosine_similarity > 0.79:
+                    rm_fp_file.write('%s,%s,%s \n'%(product,product_fp,cosine_similarity))
+
+	with open('/Users/nischalhp/Downloads/softwares/datasets/products_comparison/rm_cs.csv','w') as rm_cs_file:
+	    for product in redmart_products:
+	    	for product_cs in coldstorage_products:
+	    		product_vector = text_to_vector(product)
+	    		product_cs_vector = text_to_vector(product_cs)
+	    		cosine_similarity = get_cosine(product_vector,product_cs_vector)
+	    		if cosine_similarity > 0:
+	    			rm_cs_file.write('%s,%s,%s \n'%(product,product_cs,cosine_similarity))	
+
+
+read_text()
+
+
+
+
+
+
+
+
+
