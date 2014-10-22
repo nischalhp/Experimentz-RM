1e715175929aba612544655741ca7d37ffcca220
Nischal HP <nischalhp@Nischals-MacBook-Air.local>
2014-10-22 15:46:04 +0530

    Cosine object

---
+++ cosine_distance_objects.py
@@ -0,0 +1,17 @@
+class cosine_object:
+
+    product_name = ''
+    product_id = ''
+
+    def set_product_details(self,product_name,product_id):
+
+        self.product_name=product_name
+        self.product_id=product_id
+
+    def get_product_name(self):
+        return self.product_name
+
+    def get_product_id(self):
+        return self.product_id
+
+
